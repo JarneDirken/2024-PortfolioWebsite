0f8c56cf85d1e494007321d74f092162ef1cf672
Jarne Dirken <r0882161@student.thomasmore.be>
2024-06-13 12:05:07 +0200

    analytics and speed insights vercel

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
 
